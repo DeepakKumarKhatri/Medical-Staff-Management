7e4833c90f0cddefc0a27bffcfcb3954aca7c62f
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-03 22:25:00 +0500

    Added SignUp Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import AuthLayout from "./components/AuthLayout/AuthLayout";
+import SignUp from "./components/SignUp/SignUp";
 
 export default function App() {
   return (
-    <AuthLayout/>
-  )
+    <AuthLayout>
+      <SignUp />
+    </AuthLayout>
+  );
 }
--- client/src/components/AuthLayout/AuthLayout.jsx
+++ client/src/components/AuthLayout/AuthLayout.jsx
@@ -3,10 +3,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import TextBackground from "../TextBackground/TextBackground";
-import SignInSide from "../SignUp/SignInSide";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
 
-export default function AuthLayout() {
-
+export default function AuthLayout({ children }) {
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
       <CssBaseline />
@@ -14,7 +14,7 @@ export default function AuthLayout() {
         <TextBackground />
       </Grid>
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <SignInSide/>
+        {children}
       </Grid>
     </Grid>
   );
