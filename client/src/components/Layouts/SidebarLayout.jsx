a017f2a9363bf838b56a161215ec4fd8714aea71
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-07 11:32:40 +0500

    Fixed Major Issue of Scrollable Sidebar

--- client/src/components/Layouts/SidebarLayout.jsx
+++ client/src/components/Layouts/SidebarLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from "react-router-dom";
 
 const SidebarLayout = ({ children }) => {
   return (
-    <div className="flex">
-      {children}
-      <div className="flex-1">
+    <div className="flex h-screen">
+      <div className="fixed top-0 left-0 h-full w-64 bg-gray-800 text-white">
+        {children}
+      </div>
+      <div className="ml-64 flex-1 overflow-auto">
         <Outlet />
       </div>
     </div>
