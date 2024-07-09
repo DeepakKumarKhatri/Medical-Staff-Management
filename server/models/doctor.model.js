c1f0638c76aa091b78996a674041a41d7a92a10c
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-09 22:21:34 +0500

    Created Doctor Schema

--- server/models/doctor.model.js
+++ server/models/doctor.model.js
@@ -1,34 +1,5 @@
 const mongoose = require("mongoose");
 
-const instructionsSchema = new mongoose.Schema(
-  {
-    instructions: [{ type: String, required: true }],
-    additionalNotes: { type: String },
-  },
-  { timestamps: true }
-);
-
-const diseaseSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: { type: String },
-    diagnosedBy: {
-      type: String,
-      required: true,
-    },
-    instructions: instructionsSchema,
-    status: {
-      type: String,
-      required: true,
-      enum: ["Start-Treatment", "Mid-Treatment", "End-Treatment"],
-    },
-  },
-  { timestamps: true }
-);
-
 const systemAccessSchema = new mongoose.Schema({
   isBlocked: {
     type: Boolean,
@@ -54,7 +25,7 @@ const submissionSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const patientSchema = new mongoose.Schema(
+const doctorSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
@@ -77,15 +48,32 @@ const patientSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    yearsOfExperience: {
+      type: Number,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
     contact: {
       type: String,
       unique: true,
     },
-    diseases: [diseaseSchema],
+    patients: {
+      type: [
+        {
+          patient: {
+            type: mongoose.Types.ObjectId,
+            ref: "Patient",
+          },
+        },
+      ],
+    },
     submissions: [submissionSchema],
     systemAccess: systemAccessSchema,
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Patient", patientSchema);
+module.exports = mongoose.model("Doctor", doctorSchema);
