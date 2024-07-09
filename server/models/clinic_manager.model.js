db25037b9aa66adc35d98bf2614e8017881df197
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-07-09 22:25:44 +0500

    Created Clinic Manager Schema

---
+++ server/models/clinic_manager.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const systemAccessSchema = new mongoose.Schema({
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  profileCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const clinicManagerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    systemAccess: systemAccessSchema,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ClinicManager", clinicManagerSchema);
